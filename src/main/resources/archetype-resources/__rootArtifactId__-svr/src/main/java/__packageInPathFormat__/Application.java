0d50601f77fd77c2a0aef0fdfbad1c5f254c6f48
zbz <nnzbz@qq.com>
2020-05-27 20:25:33 +0800

    * 初步Http测试完成

--- src/main/resources/archetype-resources/__rootArtifactId__-svr/src/main/java/__packageInPathFormat__/Application.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-svr/src/main/java/__packageInPathFormat__/Application.java
@@ -4,15 +4,12 @@
 package ${package};
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.SpringCloudApplication;
 
-// 这个注解是为了使该包内的过滤器生效。
-@ServletComponentScan("rebue.sbs.smx.filter")
 @SpringCloudApplication
-public class ${projectNameCapitalise}Application {
+public class Application {
     public static void main(final String[] args) {
-        SpringApplication.run(${projectNameCapitalise}Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
