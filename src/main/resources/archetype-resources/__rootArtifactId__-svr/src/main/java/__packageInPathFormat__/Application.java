9afd832390164e184dff744eb46121906fddd722
zbz <nnzbz@21cn.com>
2020-06-04 23:29:42 +0800

    * Application.java引入Swagger3

--- src/main/resources/archetype-resources/__rootArtifactId__-svr/src/main/java/__packageInPathFormat__/Application.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-svr/src/main/java/__packageInPathFormat__/Application.java
@@ -6,6 +6,10 @@ package ${package};
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "基于Spring CLoud构建微服务的演示项目", version = "${version}"))
 @SpringCloudApplication
 public class Application {
 
