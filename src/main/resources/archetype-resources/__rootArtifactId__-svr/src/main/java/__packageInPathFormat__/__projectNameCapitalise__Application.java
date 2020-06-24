63a5340dd0177222c9d62893823402b43840ee39
zbz <nnzbz@qq.com>
2020-06-24 10:34:29 +0800

    * 优化代码

--- src/main/resources/archetype-resources/__rootArtifactId__-svr/src/main/java/__packageInPathFormat__/__projectNameCapitalise__Application.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-svr/src/main/java/__packageInPathFormat__/__projectNameCapitalise__Application.java
@@ -6,10 +6,6 @@ package ${package};
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
-@OpenAPIDefinition(info = @Info(title = "${projectDescription}", version = "${version}"))
 @SpringCloudApplication
 public class ${projectNameCapitalise}Application {
 
