a92a2f8337a2b3c86d009079c9b8ea6237445fde
heguozhan <2326375069@qq.com>
2022-01-04 09:21:22 +0800

    跟新upload

--- src/main/resources/archetype-resources/__rootArtifactId__-svr/upload.sh
+++ src/main/resources/archetype-resources/__rootArtifactId__-svr/upload.sh
@@ -10,15 +10,44 @@ fi
 SVR_FILE="/usr/local/$SVR_NAME/stack.yml"
 LOC_FILE="$CURRENT_PATH/stack.yml"
 
+echo 请选择nacos配置：
+NACOS_CONF=
+IS_TRUE=true
+while $IS_TRUE
+do
+	echo 1.单机  2.集群  3.自定义
+	read NACOS_MODLE
+	case ${NACOS_MODLE} in 
+		1) 
+		NACOS_MODLE="单机"
+		NACOS_CONF='nacos1:8848'
+		IS_TRUE=false
+		;;
+		2) 
+		NACOS_MODLE="集群"
+		IS_TRUE=false
+		;;
+		3) 
+		NACOS_MODLE="自定义"
+		read -p  "请输入nacos配置：" CU_NACOS
+		NACOS_CONF=$CU_NACOS
+		IS_TRUE=false
+		;;
+		*)
+		echo 没有该选项，请重新选择
+	esac
+done
+
 echo ===============================
 echo 参数详情
 echo 远程主机地址：$REMOTE_HOST
 echo 远程登录账户：$REMOTE_LOGIN_NAME
 echo 微服务的名称：$SVR_NAME
+echo nacos部署模式:$NACOS_MODLE
 echo ===============================
 
 #判断服务器是否存在stack.yml文件
-if ssh ${REMOTE_LOGIN_NAME}${REMOTE_HOST} test -e ${SVR_FILE};then
+if  ssh ${REMOTE_LOGIN_NAME}${REMOTE_HOST} test -e ${SVR_FILE};then
 	echo "服务器已经存在${SVR_FILE}"
 else
 	#判断本地是否存在stack.yml
@@ -44,7 +73,6 @@ else
 	fi
 fi
 
-
 rsync --progress -z target/${SVR_NAME}-*.jar ${REMOTE_LOGIN_NAME}${REMOTE_HOST}:/usr/local/${SVR_NAME}/ --exclude='*-javadoc.jar' --exclude='*-sources.jar'
 
 rsync --progress -z src/main/resources/config/bootstrap-prod.yml ${REMOTE_LOGIN_NAME}${REMOTE_HOST}:/usr/local/${SVR_NAME}/config/
@@ -61,7 +89,19 @@ if [ -d "$TEMP_DIR" ];then
  	rsync -r --progress -z $TEMP_DIR ${REMOTE_LOGIN_NAME}${REMOTE_HOST}:/usr/local/${SVR_NAME}/
 fi
 
+case ${NACOS_MODLE} in
+	'单机')
+	ssh $REMOTE_LOGIN_NAME$REMOTE_HOST "sed -i '/server-addr/s/:.*/: ${NACOS_CONF}/g' /usr/local/${SVR_NAME}/config/bootstrap-prod.yml"
+	;;
+	'集群')
+	;;
+	'自定义')
+	ssh $REMOTE_LOGIN_NAME$REMOTE_HOST "sed -i '/server-addr/s/:.*/: ${NACOS_CONF}/g' /usr/local/${SVR_NAME}/config/bootstrap-prod.yml"
+	;;
+esac
+
 if [ -f "$LOC_FILE" ];then
  	rsync --progress -z $LOC_FILE ${REMOTE_LOGIN_NAME}${REMOTE_HOST}:/usr/local/${SVR_NAME}/
+	rm -f $LOC_FILE
 fi
 echo ===============================
