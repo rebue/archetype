47f643dad71b2d86c1268e221cd5a7baf7ff70b5
zbz <nnzbz@21cn.com>
2022-02-23 13:43:27 +0800

    + @EnableWebFlux

--- src/main/resources/archetype-resources/__rootArtifactId__-svr/src/main/java/__packageInPathFormat__/__projectNameCapitalise__Application.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-svr/src/main/java/__packageInPathFormat__/__projectNameCapitalise__Application.java
@@ -4,11 +4,13 @@
 package ${package};
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringCloudApplication
 @EnableCaching
+@EnableWebFlux
 public class ${projectNameCapitalise}Application {
 
     public static void main(final String[] args) {
