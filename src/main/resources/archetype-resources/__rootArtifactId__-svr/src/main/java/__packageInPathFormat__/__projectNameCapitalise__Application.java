423c546b964ca5204aab7dec7e666a694ed0971b
zbz <nnzbz@qq.com>
2025-01-18 09:35:01 +0800

    + 服务层添加 beanSearchById 方法

--- src/main/resources/archetype-resources/__rootArtifactId__-svr/src/main/java/__packageInPathFormat__/__projectNameCapitalise__Application.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-svr/src/main/java/__packageInPathFormat__/__projectNameCapitalise__Application.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @EnableWebFlux
 // 如需访问其它微服务，请解开下面的注释
 // @EnableFeignClients
+@Slf4j
 public class ${projectNameCapitalise}Application{
 
     public static void main(final String[] args) {
@@ -15,7 +18,7 @@ public class ${projectNameCapitalise}Application{
             SpringApplication.run(${projectNameCapitalise}Application.class, args);
         } catch (Throwable e) {
             if (!e.getClass().getName().contains("SilentExitException")) {
-                e.printStackTrace();
+                log.error("${artifactId} 启动失败", e);
                 throw e;
             }
         }
