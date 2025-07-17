398159e5b6946ae97bb3169f2a226aa20e0e7fc7
zbz <nnzbz@qq.com>
2025-07-17 19:35:25 +0800

    * 修正生成代码的顺序

--- src/main/resources/archetype-resources/__rootArtifactId__-gen/src/main/java/__packageInPathFormat__/gen/__projectNameCapitalise__Gen.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-gen/src/main/java/__packageInPathFormat__/gen/__projectNameCapitalise__Gen.java
@@ -8,6 +8,7 @@ import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.exception.XMLParserException;
 
 import io.github.codgen.CodgenApplication;
+import io.github.codgen.java.CodgenJavaApplication;
 import rebue.mbgx.MybatisGeneratorWrap;
 
 /**
@@ -16,7 +17,11 @@ import rebue.mbgx.MybatisGeneratorWrap;
 public class ${projectNameCapitalise}Gen{
 
     public static void main(String[] args) throws IOException, XMLParserException, SQLException, InterruptedException, InvalidConfigurationException {
-        MybatisGeneratorWrap.gen(Path.of("target", "mbgx"), true, null);
+        // 先根据mbgx的配置模板生成配置、根据数据库生成代码
         CodgenApplication.main(new String[] { "-i", "src/main/resources/in", "-o", "../" });
+        // 再根据生成的mbgx配置生成MyBatis的代码
+        MybatisGeneratorWrap.gen(Path.of("target", "mbgx"), true, null);
+        // 最后根据解析java代码(Vo、Ctrl)生成其它的代码
+        CodgenJavaApplication.main(new String[] {});
     }
 }
